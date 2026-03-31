2fe73b02d49920c7894b5c469298897981f3f395
Brando <brandonbradleysg@gmail.com>
2026-03-31 22:22:32 +0800

    v1.0
    - Added an 'infinite' while loop for mainInput()
    - Added downloads folder
    - Had fun :-)

--- main.c
+++ main.c
@@ -10,13 +10,14 @@ int mainInput();
 void createFile(char inputLink[]){
     if(checkInputLink(inputLink)){
         int inputSize = strlen(inputLink) + 1;
-        char *cmdInput = "yt-dlp -x --audio-format mp3 --no-playlist";
+        char *cmdInput = "yt-dlp -x --audio-format mp3 --no-playlist -P \"./Downloads\"";
         char systemCmdInput[256];
         // "strlen(cmdInput) + strlen(inputLink) + 4" 4 added bytes for whitespace, quotation marks and \0.
         snprintf(systemCmdInput, strlen(cmdInput) + strlen(inputLink) + 4, "%s \"%s\"", cmdInput, inputLink);   
         system(systemCmdInput);
+        printf("Download successful!\n");
     } else {
-        printf("Please input a youtube link!");
+        printf("Please input a youtube link!\n");
     }
 }
 
@@ -37,23 +38,28 @@ int checkInputLink(char inputLink[]){
 int mainInput(){
     char *inputLink;
     char userInputLink[100];    // TBD: Check if input exceeds 100
-    printf("Brando's YT to MP3 converter\n---------------\n\nInput a youtube video link: ");
-    scanf("%s", userInputLink);
+    printf("Input a youtube video link: ");
+    fgets(userInputLink, sizeof(userInputLink), stdin);
+    if (userInputLink[0] == '\n'){
+        printf("Exiting program..");
+        exit(EXIT_SUCCESS);
+    }
 
     inputLink = calloc(strlen(userInputLink) + 1, sizeof(char));
     if (!inputLink) {
-        printf("Error initializing memory... Exiting program");
+        printf("Error initializing memory... Exiting program\n");
         return 0;
     }
     strcpy(inputLink, userInputLink);
     createFile(inputLink);
-
+    free(inputLink);
 }
 
+
 int main(){
-    int a;
-    mainInput();
-    scanf("%d", a);
-    return 0;
+    printf("Brando's YT to MP3 converter. MP3 output is located in the downloads folder.\nTo exit, enter an empty input\n---------------\n");
+    while(1 == 1){
+        mainInput();
+    }
 
 }
